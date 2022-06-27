fa3ebddb88f22ce380691db217bb96b86f4488e6
Kiko Seijo <kiko@sunnyface.com>
2022-06-27 14:56:03 +0200

    Prepare for self assesment.

--- level03/ft_list_size/ft_list_size.c
+++ level03/ft_list_size/ft_list_size.c
@@ -1,14 +1,4 @@
-// Passed Moulinette 2019.09.01
 
-#include "ft_list.h"
-
-int	ft_list_size(t_list *begin_list)
-{
-	if (begin_list == 0)
-		return (0);
-	else
-		return (1 + ft_list_size(begin_list->next));
-}
 
 //---------------------------------------------------------------
 // #include <stdio.h>
