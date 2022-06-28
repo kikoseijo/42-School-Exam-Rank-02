5faa5fda1635190a9ddb677d1070d638cae3a10e
Kiko Seijo <kiko@sunnyface.com>
2022-06-28 22:37:55 +0200

    Done ft_list_size-

--- level03/ft_list_size/ft_list_size.c
+++ level03/ft_list_size/ft_list_size.c
@@ -1,24 +1,37 @@
+#include "ft_list.h"
 
+int	ft_list_size(t_list *begin_list)
+{
+  int i = 0;
 
-//---------------------------------------------------------------
-// #include <stdio.h>
-// #include <stdlib.h>
+  while(begin_list)
+  {
+    i++;
+    begin_list = begin_list->next;
+  }
+  return (i);
+}
 
-// int	main(void)
-// {
-// 	int n = 0;
-
-// 	t_list *c = malloc(sizeof(t_list));
-// 	c->next = 0;
-// 	c->data = &n;
-
-// 	t_list *b = malloc(sizeof(t_list));
-// 	b->next = c;
-// 	b->data = &n;
-
-// 	t_list *a = malloc(sizeof(t_list));
-// 	a->next = b;
-// 	a->data = &n;
-
-// 	printf("%d\n", ft_list_size(a));
-// }
+/*
+** #include <stdio.h>
+** #include <stdlib.h>
+**
+** int	main(void)
+** {
+** 	int n = 0;
+**
+** 	t_list *c = malloc(sizeof(t_list));
+** 	c->next = 0;
+** 	c->data = &n;
+**
+** 	t_list *b = malloc(sizeof(t_list));
+** 	b->next = c;
+** 	b->data = &n;
+**
+** 	t_list *a = malloc(sizeof(t_list));
+** 	a->next = b;
+** 	a->data = &n;
+**
+** 	printf("%d\n", ft_list_size(a));
+** }
+*/
