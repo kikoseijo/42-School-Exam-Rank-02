0cc073b92981f801d138312ad5261cd1113c1fa3
jseijo-p <jseijo-p@student.42malaga.com>
2022-06-30 10:44:19 +0200

    Dont sort_list.

--- level04/sort_list/sort_list.c
+++ level04/sort_list/sort_list.c
@@ -1,74 +1,83 @@
-// Passed Moulinette 2019.09.01
-
 #include "list.h"
 
-void	swap_values(t_list *a, t_list *b)
+void swap_vals(int *a, int *b)
 {
-	int swap = a->data;
-	a->data = b->data;
-	b->data = swap;
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
 }
 
 t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
 {
-	int swapped = 1;
-	t_list *cur = lst;
+	t_list *cur;
+	t_list *out;
+	t_list *temp;
 
-	while (swapped == 1)
+
+	out = lst;
+	temp = lst;
+	while(out)
 	{
-		swapped = 0;
-		while (cur != 0 && cur->next != 0)
+		cur = out;
+		while(cur && cur->next)
 		{
-			if (cmp(cur->data, cur->next->data) == 0)
+			if(cmp(cur->data, cur->next->data) == 0)
 			{
-				swap_values(cur, cur->next);
-				swapped = 1;
+				swap_vals(&cur->data, &cur->next->data);
+				cur = temp;
 			}
-			cur = cur->next;
+			else
+					cur = cur->next;
 		}
-		cur = lst;
+		out = out->next;
 	}
 	return (lst);
 }
 
-//----------------------------------------------------------------
-// #include <stdio.h>
-// #include <stdlib.h>
+// kcc sort_list/sort_list.c && ./a.out && rm -rf a.out
 
-// int ascending(int a, int b)
-// {
-// 	return (a <= b);
-// }
 
-// int	main(void)
-// {
-// 	t_list *c = malloc(sizeof(t_list));
-// 	c->next = 0;
-// 	c->data = 45;
-
-// 	t_list *b = malloc(sizeof(t_list));
-// 	b->next = c;
-// 	b->data = 73;
-
-// 	t_list *a = malloc(sizeof(t_list));
-// 	a->next = b;
-// 	a->data = 108;
-
-// 	t_list *cur = a;
-// 	while (cur)
-// 	{
-// 		printf("%d, ", cur->data);
-// 		cur = cur->next;
-// 	}
-// 	printf("\n");
-
-// 	cur = sort_list(a, ascending);
-
-// 	// cur = a;
-// 	while (cur)
-// 	{
-// 		printf("%d, ", cur->data);
-// 		cur = cur->next;
-// 	}
-// 	printf("\n");
-// }
+/*
+** #include <stdio.h>
+** #include <stdlib.h>
+**
+** int ascending(int a, int b)
+** {
+** 	return (a <= b);
+** }
+**
+** int	main(void)
+** {
+** 	t_list *c = malloc(sizeof(t_list));
+** 	c->next = 0;
+** 	c->data = 45;
+**
+** 	t_list *b = malloc(sizeof(t_list));
+** 	b->next = c;
+** 	b->data = 73;
+**
+** 	t_list *a = malloc(sizeof(t_list));
+** 	a->next = b;
+** 	a->data = 108;
+**
+** 	t_list *cur = a;
+** 	while (cur)
+** 	{
+** 		printf("%d, ", cur->data);
+** 		cur = cur->next;
+** 	}
+** 	printf("\n");
+**
+** 	cur = sort_list(a, ascending);
+**
+** 	// cur = a;
+** 	while (cur)
+** 	{
+** 		printf("%d, ", cur->data);
+** 		cur = cur->next;
+** 	}
+** 	printf("\n");
+** }
+*/
